7a013875a97d2132f229a6909848d052098da40c
danitt030 <139822527+danitt030@users.noreply.github.com>
2025-05-04 14:48:09 -0600

    feat(product): Falto subir archivo SWAGGER

---
+++ configs/swagger.js
@@ -0,0 +1,39 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Warehouse Management API",
+            version: "1.0.0",
+            description:
+                "El objetivo de este proyecto es desarrollar una aplicación web para la administración de una almacenadora de productos, además de la implementación efectiva del Stack MERN (MongoDB, Express, React, Node.js) para lograr un sistema de administración de almacenadora funcional y escalable.",
+            contact: {
+                name: "IN6BM_Group5",
+                email: "anthonyescobarponce@Outlook.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://127.0.0.1:3001/warehouseManagement/v1",
+            },
+        ],
+        components: {
+            securitySchemes: {
+                BearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+        },
+    },
+    apis: [
+        "./src/product/product.routes.js"
+    ],
+};
+
+const swaggerDocs = swaggerJSDoc(options);
+
+export { swaggerDocs, swaggerUi };
